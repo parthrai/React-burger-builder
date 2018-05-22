33ff6fe2ebe1ab86e4b2f8d719f8f3c3567caa88
Parth Sharma <parthrai2@gmail.com>
2018-05-21 20:31:35 -0400

    checkout modal added

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import classes from './Modal.css';
+import Aux from '../../../hoc/a'
+import Backdrop from '../Backdrop/Backdrop'
 
 const modal = (props) =>(
+
+    <Aux>
+
+        <Backdrop show={props.show}
+                  clicked={props.modalClosed}
+
+        />
 
     <div
         className={classes.Modal}
@@ -13,6 +22,7 @@ const modal = (props) =>(
         {props.children}
     </div>
 
+    </Aux>
 );
 
 export default modal;
